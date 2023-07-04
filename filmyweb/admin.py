119c4e6c0ba737316261d6848ec655cf85f1a43d
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-04 11:50:10 +0200

    add new project from Udemy

--- filmyweb/admin.py
+++ filmyweb/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Film
 
 # Register your models here.
-admin.site.register(Film)
+# admin.site.register(Film)
+
+@admin.register(Film) # rejestrujemy film; dekorator
+class FilmAdmin(admin.ModelAdmin):
+    # fields = ['tytuł', 'opis', 'rok']
+    # exclude = ['opis']
+    list_display = ['tytuł', 'imdb_rating', "rok"]
+    list_filter = ('rok',)
+    search_fields = ('tytuł','opis')
+
