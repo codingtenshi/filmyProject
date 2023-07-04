a0ec26894d2c44e489963af7133e7a7ba6c76602
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-04 13:42:25 +0200

    add templates directory

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def test_response(request):
-    return HttpResponse("To jest nasz pierwszy test")
+def wszystkie_filmy(request):
+    # return HttpResponse('<h1>To jest nasz pierwszy test<h1>')
+    return render(request, 'filmy.html')
