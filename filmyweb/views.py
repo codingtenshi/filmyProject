47e0fe2d2b39e053752281117ca247540e2fcce6
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-05 11:09:22 +0200

    developing templates

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Film
 
 # Create your views here.
 def wszystkie_filmy(request):
-    # return HttpResponse('<h1>To jest nasz pierwszy test<h1>')
-    return render(request, 'filmy.html')
+    # test = 'To jest coś wewnątrz views'
+    # return render(request, 'filmy.html', {'filmy' : ['Avatar', 'Titanic']})
+    # wszystkie = Film.objects.all()
+    wszystkie = []  # wyskoczy na stronie Brak filmów
+    return render(request, 'filmy.html', {'filmy': wszystkie})
