39b5229505ccba179fdf5cce789da7f15728fcb4
Karina Gornicka <karinagornicka@Lukaszs-MacBook-Pro.local>
2023-07-11 14:30:54 +0200

    add logout function

--- filmy/settings.py
+++ filmy/settings.py
@@ -122,6 +122,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'moje_media'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'wszystkie_filmy'
+LOGOUT_REDIRECT_URL = 'login'
 
 
 # Default primary key field type
--- filmy/urls.py
+++ filmy/urls.py
@@ -23,5 +23,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('filmy/', include('filmyweb.urls')),
-    path('login/', auth_views.LoginView.as_view()),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
